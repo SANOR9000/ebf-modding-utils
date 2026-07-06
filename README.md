eae181aacac3ea50a6fe38212928faa795eeb775
SANOR9000 <sanor9000@gmail.com>
2026-07-06 17:08:09 -0300

    Added give-all-festival-exclusives and updated README.md

--- README.md
+++ README.md
@@ -13,3 +13,13 @@ Also reworks Rune Claw to use Akron's orb regardless of the latter's position or
 
 ### redirect-sendHits-to-magicGraphics
 Remember the 4 copies of ```function luckystar()```? Now you can make them always redirect to magicGraphics instead
+
+## Misc
+
+### give-all-festival-exclusives
+Drop in a ```input.meow``` file, run the script and an ```output.meow``` file will be created with all equipments and skills tied to festivals given to you
+It also gives you 69000 red envelopes so you can upgrade the Coin Charm and the Lucky Fish
+
+### respawn-devourer-encounter
+Works the same way as above
+Name's self explainatory
---
+++ misc/give-all-festival-exclusives/main.py
@@ -0,0 +1,45 @@
+import base64
+import json
+
+
+def open_save():
+    with open(r"input.meow", "r") as f:
+        data = f.read()
+
+    data = data.encode()
+    data = base64.b64decode(data)
+    data = json.loads(data)
+    return data
+
+
+# lmao
+def save_save(data):
+    data = json.dumps(data)
+    data = data.encode()
+    data = base64.b64encode(data)
+    data = data.decode()
+
+    with open(r"output.meow", "w") as f:
+        f.write(data)
+
+
+data = open_save()
+
+data["equipOwnage"][228] = True      # coincharm
+data["equipOwnage"][227] = True      # luckyfish
+data["equipOwnage"][204] = True      # loveblade
+data["equipOwnage"][36] = True       # heartpendant
+data["equipOwnage"][141] = True      # heavensvoice
+data["equipOwnage"][183] = True      # slimestaff
+data["equipOwnage"][98] = True       # coatofteeth
+data["equipOwnage"][119] = True      # spidergown
+data["equipOwnage"][56] = True       # pumpkinhead
+data["equipOwnage"][77] = True       # spiderbobble
+data["equipOwnage"][99] = True       # santaoutfit
+data["equipOwnage"][120] = True      # santaskirt
+data["equipOwnage"][57] = True       # santahat
+data["equipOwnage"][78] = True       # hollyhairpin
+data["spellOwnage"][179] = True      # firecrackers
+data["itemQuantities"][125] = 69000  # Red Envelope
+
+save_save(data)
